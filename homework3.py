733987741dba657e51ac0961a07171960425400a
Руслан <xamet@icloud.com>
2024-06-20 19:31:25 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,9 @@
+a = 'Ruslan'
+print(a)
+b = (28)
+print(b)
+b = (28 + 1)
+print(b)
+is_student = "True"
+print(is_student)
+print('name:' , a ,'Age:' , b , 'New age:' , b , 'Is Student:' , is_student)
